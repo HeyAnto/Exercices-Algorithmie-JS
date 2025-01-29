970d3caace7a56f1da828c22659da511835936b0
HeyAnto <antonin63170@gmail.com>
2025-01-29 16:01:48 +0100

    Exercice 0.4

---
+++ exercices/0.4/0.4.js
@@ -0,0 +1,8 @@
+let age = 21;
+let height = 175;
+
+if (age >= 18 && height >= 140) {
+  console.log("Je peux rentrer dans le manège.");
+} else {
+  console.log("Je n'ai pas le droit de rentrer dans le manège.");
+}
